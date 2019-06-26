d2812ba621b401e73fbbb3d47d0bead40e965e56
Nikita Borisowsky <yuzirock@gmail.com>
2019-06-26 11:25:03 +0200

    next@8.1.1-canary.60

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
-  experimental: {
-    dynamicRouting: true,
-  },
   env: {
     APP_ENV: process.env.APP_ENV || 'development',
   },
