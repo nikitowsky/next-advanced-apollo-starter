223728bf6cbb3d6167112941530f780595829b8d
Nikita Borisowsky <yuzirock@gmail.com>
2019-06-27 21:09:31 +0200

    Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { NextPage } from 'next';
 import Link from 'next/link';
 
-const Index: NextPage = () => (
+const Index = () => (
   <div>
-    Hello world! You're running server in <i>{process.env.APP_ENV}</i> mode! <br />
-    You can set-up <b>process.env.APP_ENV</b> variable in <b>next.config.js</b>. <br />
+    Hello world! You're running server in <i>{process.env.APP_ENV}</i> mode!
+    <br />
+    You can set-up <b>process.env.APP_ENV</b> variable in <b>next.config.js</b>.
+    <br />
     Here is base routing:{' '}
     <Link href="/users">
       <a>List of users...</a>
