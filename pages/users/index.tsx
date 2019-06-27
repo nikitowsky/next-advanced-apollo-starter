191659cb2bc0d3bd7b0204a0eed006b53db74cca
Nikita Borisowsky <yuzirock@gmail.com>
2019-06-27 21:08:22 +0200

    Fix dynamic route link

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { NextPage } from 'next';
 import Link from 'next/link';
 
-const Users: NextPage = () => (
+const Users = () => (
   <div>
     Dynamic routes included! Example:{' '}
-    <Link href="/users/$user" as="/users/nikita">
+    <Link href="/users/[user]" as="/users/nikita">
       <a>Nikita</a>
     </Link>
   </div>
