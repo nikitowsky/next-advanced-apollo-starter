5899c4a51c19c4ab931d19d37b75d0b9825aa0cc
Nikita Borisowsky <yuzirock@gmail.com>
2019-07-02 18:58:21 +0200

    Update tests

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -4,7 +4,7 @@ import Index from '../../pages/index';
 
 afterEach(cleanup);
 
-it('Should render Index page without crash', () => {
+it('Should render index page without crash', () => {
   const { getByText } = render(<Index />);
 
   expect(getByText(/Hello world!/i)).toBeTruthy();
