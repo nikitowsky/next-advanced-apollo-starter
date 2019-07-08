045d43b43c0196d90b78d3797e11052bb53c6bce
Nikita Borisowsky <yuzirock@gmail.com>
2019-07-08 13:31:53 +0200

    Refactor tests files structure

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/utils/setupTests.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/__tests__/__mocks__/',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
 };
