3c7c249eb1664af6955b1c25c8625b70fff7bf0e
Nikita Borisowsky <yuzirock@gmail.com>
2019-07-08 13:21:28 +0200

    :tada: Fix tests

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,12 +1,18 @@
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
 
 import Index from '../../pages/index';
+import I18Next from '../../utils/__mocks__/i18n';
 
 afterEach(cleanup);
 
 // FIXME: Component with localization should be correcly tested
-it('Should render index page without crash', () => {
-  const { getByText } = render(<Index />);
+it('Should render index page without crash', async () => {
+  const { getByText } = render(
+    <I18nextProvider i18n={I18Next}>
+      <Index />
+    </I18nextProvider>,
+  );
 
-  expect(getByText(/Hello world!/i)).toBeTruthy();
+  await waitForElement(() => getByText(/Hello world!/i));
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/utils/setupTests.js'],
 };
