c33dacb8d06e8772f905a673167e7f51869c7e06
Nikita Borisowsky <yuzirock@gmail.com>
2020-03-10 23:47:11 +0200

    Update configs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/tests/__helpers__/cssTransform.js',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   env: {
-    // GRAPHQL_URL: 'https://example.com/graphql',
-    GRAPHQL_URL: 'https://stage-api.neovip.one/graphql',
+    GRAPHQL_URL: 'https://example.com/graphql',
   },
 };
