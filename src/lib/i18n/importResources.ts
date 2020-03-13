2459476a4b8a4ec3945f9c1b67c2a7e010351ec5
Nikita Borisowsky <yuzirock@gmail.com>
2020-03-13 20:17:49 +0200

    .ts locales -> .json locales

--- src/lib/i18n/importResources.ts
+++ src/lib/i18n/importResources.ts
@@ -1,15 +1,15 @@
-import NameSpace from "./nameSpaces";
+import NameSpace from './nameSpaces';
 
 const importResources = (languages: string[], namespaces: NameSpace[]) => {
   const resources = {};
 
   for (const language of languages) {
     for (const namespace of namespaces) {
-      const locale = require(`../../locales/${language}/${namespace}`);
+      const locale = require(`../../locales/${language}/${namespace}.json`);
 
       resources[language] = {
         ...resources[language],
-        [namespace]: locale.default
+        [namespace]: locale,
       };
     }
   }
--- src/lib/i18n/nameSpaces.ts
+++ src/lib/i18n/nameSpaces.ts
@@ -1,5 +1,5 @@
 enum NameSpace {
-  COMMON = "common"
+  COMMON = 'common',
 }
 
 export default NameSpace;
